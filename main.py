dcf5fff1cb7e5f7d44bed07f6199a46490e24b1c
gabrantes <gabrantes99@gmail.com>
2017-04-15 11:24:09 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-import 
+import random
+from classes.events import Event
+
+print('Welcome to the game!')
+
+energy = 100
+
+# Defining Events
+low_air = Event("Low Air", "ghA")
+asteroid = Event("Asteroid", "aRu")
+black_hole = Event("Black hole", "Oyn")
+
+event_list = [low_air, asteroid, black_hole]
+
+while energy > 0:
+
+    print('Current energy level is ' + str(energy) + '%')
+
+    chance = random.randint(0, 2)
+
+    event_list[chance].show()
+
+    if energy <= 0:
+
+        print('Game Over! Your ship sustained too much damage.')
+
+    break
