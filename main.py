2209e985da1df70909e78a2dc930f0196586e5fc
gabrantes <gabrantes99@gmail.com>
2017-04-15 13:08:29 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from classes.game import Energy
 
 running = True
 energy = Energy(100)
+score = 0
 
 instructions = """
 ********************************************************************************
@@ -21,10 +22,16 @@ this game, you will need to enter a random key code
 in order to pass the problem unhurt.
 """
 
+# Event Key-Codes
+low_air_keys = ["air", "aero", "oxy"]
+asteroid_keys = ["rocks", "meteor", "meteorite", "collide"]
+black_hole_keys = ["wormhole", "blackhole", "timespace", "gravity"]
+
+
 # Defining Events
-low_air = Event("Low Air", 3, 7)
-asteroid = Event("Asteroid", 10, 20)
-black_hole = Event("Black hole", 15, 30)
+low_air = Event("Low Air", 3, 7, low_air_keys)
+asteroid = Event("Asteroid", 10, 20, asteroid_keys)
+black_hole = Event("Black hole", 15, 30, black_hole_keys)
 
 # Creating a list of events
 event_list = [low_air, asteroid, black_hole]
@@ -37,12 +44,11 @@ while running:
     if energy.current <= 0:
         print('\nGame Over! Your ship sustained too much damage.\n')
         energy.display()
+        print("Your score is " + score)
         running = False
         break
 
     energy.display()
-
-    #print(bcolors.OKBLUE + '\nCurrent energy level is ' + str(energy) + '%' + bcolors.ENDC)
 
     chance = random.randint(0, num_events) # Creates random number
 
@@ -52,9 +58,11 @@ while running:
     if current_event.key_input(): # Checks if correct key solution was entered
         print("Problem solved!")
         print("Great work!")
+        score += 10
         continue
     else:
         energy.current -= current_event.dmg
         print("Incorrect!")
         print("You have lost " + str(current_event.dmg) + " energy")
+        score += 1
         continue
