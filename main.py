bc82316b833fac165861130ccd7916256e1c1a52
gabrantes <gabrantes99@gmail.com>
2017-04-15 11:41:26 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -11,17 +11,23 @@ asteroid = Event("Asteroid", "aRu")
 black_hole = Event("Black hole", "Oyn")
 
 event_list = [low_air, asteroid, black_hole]
+num_events = len(event_list) # number of events
 
 while energy > 0:
 
-    print('Current energy level is ' + str(energy) + '%')
+    print('\nCurrent energy level is ' + str(energy) + '%')
 
-    chance = random.randint(0, 2)
+    chance = random.randint(0, num_events) # Creates random number
 
-    event_list[chance].show()
+    event_list[chance].show() # Prints the current event
+
+    if event_list[chance].key_input(): # Checks if correct key solution was entered
+        print("Problem solved!")
+        print("Great work!")
+    else:
+        energy -= 10
+        print("Incorrect!")
+        print("You have lost energy")
 
     if energy <= 0:
-
         print('Game Over! Your ship sustained too much damage.')
-
-    break
