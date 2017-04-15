3175e567a3288936c088ed12f79b73e43bd55a45
gabrantes <gabrantes99@gmail.com>
2017-04-15 11:41:46 -0500

    Add files via upload

--- classes/events.py
+++ classes/events.py
@@ -4,6 +4,10 @@ class Event:
         self.name = name
         self.key = key
 
-    def show(self):
+    def show(self): # Prints the current event and the solution
         print(self.name + "!!!")
         print("Type " + self.key + " to solve the problem")
+
+    def key_input(self): #
+        key_input = input()
+        return key_input == self.key
