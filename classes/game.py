1f7c8793e425b8de7e9c5556ff4e515fe9d92f06
gabrantes <gabrantes99@gmail.com>
2017-04-15 12:49:37 -0500

    Add files via upload

--- classes/game.py
+++ classes/game.py
@@ -1,15 +1,5 @@
 import sys
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLING = '\033[4m'
-
 class Energy():
     def __init__(self, current):
         self.current = current
@@ -17,11 +7,14 @@ class Energy():
 
     def display(self):
         i = 0
+        if self.current <= 0:
+            self.current = 0
         print("\nCurrent Energy: " + str(self.current) + "/100")
         while i < (self.current/5):
-            sys.stdout.write(bcolors.OKGREEN + "█" + bcolors.ENDC)
+            sys.stdout.write("█")
             i += 1
         while i < 20:
-            sys.stdout.write(bcolors.FAIL + "█" + bcolors.ENDC)
+            sys.stdout.write("_")
             i += 1
+        sys.stdout.write("|")
         print("\n")
